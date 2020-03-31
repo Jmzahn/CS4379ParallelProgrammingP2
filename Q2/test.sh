3b6c7dff5e94c4cfc304c3a1bbbda3b878fa7edc
Jacob Zahn <jmzahn@satx.rr.com>
2020-03-31 16:31:54 -0500

    modify tests

--- Q2/test.sh
+++ Q2/test.sh
@@ -9,5 +9,11 @@ echo -e "\nTest with N: 1000 procs: 8\n"
 ./guass 1000 8 > guass_1000_8_out
 echo -e "\nTest with N: 1000 procs: 12\n"
 ./guass 1000 12 > guass_1000_12_out
-echo -e "\nTest with N: 2000 procs: 12\n"
-./guass 2000 12 > guass_2000_12_out
+echo -e "\nTest with N: 2000 procs: 1\n"
+./guass 2000 1 > guass_2000_1_out
+echo -e "\nTest with N: 2000 procs: 2\n"
+./guass 2000 2 > guass_2000_2_out
+echo -e "\nTest with N: 2000 procs: 4\n"
+./guass 2000 4 > guass_2000_4_out
+echo -e "\nTest with N: 2000 procs: 8\n"
+./guass 2000 8 > guass_2000_8_out
