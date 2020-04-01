827c5003cde2e0906aa41f52c4da97c0eadc3040
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-01 01:25:39 -0500

    add args and driving logic from ex7_3

--- Q1/ex7_7/ex7_7.c
+++ Q1/ex7_7/ex7_7.c
@@ -3,6 +3,15 @@
     with code added from sec 7.8.1
 */
 #include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/time.h>
+
+#define INT_MAX 2147483647
+
 void *find_min_rw(void *list_ptr);
 pthread_mutex_t minimum_value_lock;
 int minimum_value, partial_list_size;
@@ -15,6 +24,8 @@ typedef struct {
     int pending_writers;
     pthread_mutex_t read_write_lock; 
 } mylib_rwlock_t;
+
+mylib_rwlock_t read_write_lock;
 
 void mylib_rwlock_init (mylib_rwlock_t *l) {
     l -> readers = l -> writer = l -> pending_writers = 0;
@@ -47,7 +58,7 @@ void mylib_rwlock_wlock(mylib_rwlock_t *l) {
             &(l -> read_write_lock));
     }
     l -> pending_writers --;
-    l -> writer ++ 
+    l -> writer ++;
      pthread_mutex_unlock(&(l -> read_write_lock));
 }
 
@@ -69,19 +80,93 @@ void mylib_rwlock_unlock(mylib_rwlock_t *l) {
         pthread_cond_broadcast(&(l -> readers_proceed)); 
 } 
 
-main(){
+int main(int argc, char *argv[]){
     /* declare and initialize data structures and list*/
-    minimum_value = MIN_INT;
-    pthread_init();
+
+    /* Timing variables */
+    struct timeval etstart, etstop;  /* Elapsed times using gettimeofday() */
+    struct timezone tzdummy;
+    clock_t etstart2, etstop2;  /* Elapsed times using times() */
+    unsigned long long usecstart, usecstop;
+    struct tms cputstart, cputstop;  /* CPU times for my processes */
+
+    minimum_value = INT_MAX;
+    int numP;//number of processors
+
+    //pthread_init();
     pthread_mutex_init(&minimum_value_lock, NULL);
 
     /* initialize lists, list_ptr, and partial_list_size */
     /* create and join threads here */
+    if(argc != 2){
+        return(-1);
+    }
+    else{
+        char *p;
+        long conv = strtol(argv[1], &p, 10);
+        numP = conv;
+        pthread_t finders[numP];
+        void* list_ptrs[numP];
+        double remainder = 100000000%numP;
+        if(remainder!=0.0){
+            return(-1);
+        }
+        int* randArray = (int*) malloc(100000000 * sizeof(int));
+        srand(time(NULL));
+        for(int i = 0; i < 100000000; i++){
+            randArray[i] = (int)rand() / 32768;
+        }
+        
+        int split = 100000000/numP;
+        partial_list_size = split;
+        
+        /* Start Clock */
+        printf("\nStarting clock.\n");
+        gettimeofday(&etstart, &tzdummy);
+        etstart2 = times(&cputstart);
+        
+        for(int i = 0; i < numP; i++){
+            list_ptrs[i] = &randArray[i*split];
+            pthread_create(&finders[i],NULL,find_min_rw,list_ptrs[i]);
+        }
+        for(int i = 0; i < numP; i++){
+            pthread_join(finders[i],0);
+        }
+
+        /* Stop Clock */
+        gettimeofday(&etstop, &tzdummy);
+        etstop2 = times(&cputstop);
+        printf("Stopped clock.\n");
+        usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
+        usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
+        /* Display timing results */
+        printf("\nElapsed time = %g ms.\n",
+        	 (float)(usecstop - usecstart)/(float)1000);
+        printf("(CPU times are accurate to the nearest %g ms)\n",
+        	 1.0/(float)CLOCKS_PER_SEC * 1000.0);
+        printf("My total CPU time for parent = %g ms.\n",
+        	 (float)( (cputstop.tms_utime + cputstop.tms_stime) -
+        		  (cputstart.tms_utime + cputstart.tms_stime) ) /
+        	 (float)CLOCKS_PER_SEC * 1000);
+        printf("My system CPU time for parent = %g ms.\n",
+        	 (float)(cputstop.tms_stime - cputstart.tms_stime) /
+        	 (float)CLOCKS_PER_SEC * 1000);
+        printf("My total CPU time for child processes = %g ms.\n",
+        	 (float)( (cputstop.tms_cutime + cputstop.tms_cstime) -
+        		  (cputstart.tms_cutime + cputstart.tms_cstime) ) /
+        	 (float)CLOCKS_PER_SEC * 1000);
+            /* Contrary to the man pages, this appears not to include the parent */
+        printf("--------------------------------------------\n");
+        printf("Min: %i", minimum_value);
+        free(randArray);
+    }
+    
+    return 0;
 }
 
 void *find_min_rw(void *list_ptr) {
     int *partial_list_pointer, my_min, i;
-    my_min = MIN_INT;
+    my_min = INT_MAX;
     partial_list_pointer = (int *) list_ptr;
     for (i = 0; i < partial_list_size; i++) 
         if (partial_list_pointer[i] < my_min) 
