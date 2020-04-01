fc063a2f7da786138a7e00235bc071350f108cc0
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-01 00:59:55 -0500

    add min output

--- Q1/ex7_3/ex7_3.c
+++ Q1/ex7_3/ex7_3.c
@@ -91,6 +91,7 @@ int main(int argc, char *argv[]){
         	 (float)CLOCKS_PER_SEC * 1000);
             /* Contrary to the man pages, this appears not to include the parent */
         printf("--------------------------------------------\n");
+        printf("Min: %i", minimum_value);
         free(randArray);
     }
     
