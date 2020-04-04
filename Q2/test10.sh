a24cb60e454b26a46992cd92233d7da48d7843d6
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-03 22:39:04 -0500

    update test10

--- Q2/test10.sh
+++ Q2/test10.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 echo -e "\nFor seeing values of input/output\n"
 echo -e "\nTest with N: 2 procs: 1\n"
-./guass 2 1 > ./test10/guass_2_1_out
+./guass 2 1 > ./test10out/guass_2_1_out
 echo -e "\nTest with N: 2 procs: 2\n"
-./guass 2 2 > ./test10/guass_2_2_out
+./guass 2 2 > ./test10out/guass_2_2_out
 echo -e "\nTest with N: 4 procs: 1\n"
-./guass 4 1 > ./test10/guass_4_1_out
+./guass 4 1 > ./test10out/guass_4_1_out
 echo -e "\nTest with N: 4 procs: 2\n"
-./guass 4 2 > ./test10/guass_4_2_out
+./guass 4 2 > ./test10out/guass_4_2_out
 echo -e "\nTest with N: 10 procs: 1\n"
-./guass 10 1 > ./test10/guass_10_1_out
+./guass 10 1 > ./test10out/guass_10_1_out
 echo -e "\nTest with N: 10 procs: 2\n"
-./guass 10 2 > ./test10/guass_10_2_out
-echo -e "\nTest with N: 10 procs: 4\n"
-./guass 10 4 > ./test10/guass_10_4_out
+./guass 10 2 > ./test10out/guass_10_2_out
