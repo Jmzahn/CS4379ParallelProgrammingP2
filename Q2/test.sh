4eedddba3f1c8d72ef9d98528fc1127c0ce3242f
Jacob Zahn <jmzahn@satx.rr.com>
2020-05-10 18:21:34 -0500

    fix more typos

--- Q2/test.sh
+++ Q2/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo -e "\nTest with N: 2000 procs: 1\n"
-./guass 2000 1 > ./testout/guass_2000_1_out
+./gauss 2000 1 > ./testout/gauss_2000_1_out
 echo -e "\nTest with N: 2000 procs: 2\n"
-./guass 2000 2 > ./testout/guass_2000_2_out
+./gauss 2000 2 > ./testout/gauss_2000_2_out
 echo -e "\nTest with N: 2000 procs: 4\n"
-./guass 2000 4 > ./testout/guass_2000_4_out
+./gauss 2000 4 > ./testout/gauss_2000_4_out
 echo -e "\nTest with N: 2000 procs: 8\n"
-./guass 2000 8 > ./testout/guass_2000_8_out
+./gauss 2000 8 > ./testout/gauss_2000_8_out
